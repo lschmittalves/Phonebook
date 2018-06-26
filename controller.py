738a362f52377aebf089bc34d743c522f1499918
Leonardo Alves <lschmittalves@gmail.com>
2018-06-26 16:31:28 -0300

    app finalizado para testes

--- controller.py
+++ controller.py
@@ -1,4 +1,6 @@
 from cement.core.controller import CementBaseController, expose
+from .repository import PhonebookRepository
+from .models import PhoneBookItem
 
 
 class PhonebookShellController(CementBaseController):
@@ -8,26 +10,64 @@ class PhonebookShellController(CementBaseController):
         arguments = [
             (['-n', '--name'],
              dict(action='store', help='Name')),
-            (['-t', '--tel'],
-             dict(action='store', help='Telephone')),
+            (['-p', '--phone'],
+             dict(action='store', help='Phone')),
         ]
+
+    __repository = {}
+
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw)
+        self.__repository = PhonebookRepository()
 
     @expose(hide=True)
     def default(self):
         self.list()
 
+    @expose(aliases=['f'], help="This command find a existent item to the phonebook by phone")
+    def edit(self):
+        phonebook_item = self.__repository.get_by_phone(self.app.pargs.phone)
+
+        if not phonebook_item:
+            self.app.log.info(f'Item with phone number {self.app.pargs.phone} was not found')
+            return
+
+        self.app.log.info(f'Name: {phonebook_item.name} Phone: {phonebook_item.phone}')
+
     @expose(aliases=['a'], help="This command add a new item to the phonebook")
     def add(self):
-        self.app.log.info("Inside add")
+        if not self.app.pargs.name:
+            self.app.log.warning("Provide the --name parameter")
+        if not self.app.pargs.phone:
+            self.app.log.warning("Provide the --phone parameter")
+
+        phonebook_item = PhoneBookItem(self.app.pargs.name, self.app.pargs.phone)
+
+        self.__repository.add(phonebook_item)
+
+        self.app.log.info(f'Item with phone number {self.app.pargs.phone} was add')
 
     @expose(aliases=['e'], help="This command edit a existent item to the phonebook by phone")
     def edit(self):
-        self.app.log.info("Inside edit")
+        phonebook_item = self.__repository.get_by_phone(self.app.pargs.phone)
+
+        if not phonebook_item:
+            self.app.log.info(f'Item with phone number {self.app.pargs.phone} was not found')
+            return
+
+        phonebook_item.name = self.app.pargs.name
+
+        self.app.log.info(f'Item with phone number {self.app.pargs.phone} was update')
 
     @expose(aliases=['d'], help="This command delete a existent item to the phonebook by phone")
     def delete(self):
-        self.app.log.info("Inside delete")
+        self.__repository.delete(self.app.pargs.phone)
+        self.app.log.info(f'Item with phone number {self.app.pargs.phone} was remove')
 
     @expose(aliases=['l'], help="This command list the existents itens of the phonebook")
     def list(self):
-        self.app.log.info("Inside list")
+        for phonebook_item in self.__repository.objects:
+            self.app.log.info(f'Name: {phonebook_item.name} Phone: {phonebook_item.phone}')
+
+
+
--- repository.py
+++ repository.py
@@ -1,9 +1,11 @@
-from .models import PhoneBookItem
 import json
 
 
 class PhonebookRepository:
     objects = []
+
+    def __init__(self):
+        self.__load()
 
     def __load(self):
         with open("phonebook.json", "r") as read_file:
@@ -13,20 +15,21 @@ class PhonebookRepository:
         with open("phonebook.json", "w+") as write_file:
             json.dump(self.objects, write_file)
 
+    def get_by_phone(self, phone):
+        return next(x for x in self.objects if x.phone == phone)
+
     def add(self, phonebook_item):
-        self.__load()
         self.objects.append(phonebook_item)
         self.__commit()
         return
 
     def edit(self, phonebook_item):
-        self.__load()
-        phone_item = next(for x in self.objects if x.phone == phonebook_item.phone)
+        existent_phone_item = next(x for x in self.objects if x is phonebook_item)
+        existent_phone_item.name = phonebook_item.name
         self.__commit()
         return
 
     def delete(self, phone):
-        self.__load()
         self.objects[:] = [x for x in self.objects if x.phone != phone]
         self.__commit()
         return
